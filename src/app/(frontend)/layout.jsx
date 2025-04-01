6a94e9474580423039024e76175dbf95165fafa2
Alan Silva <alanwvsilva@gmail.com>
2025-04-01 23:10:38 +0100

    add sitemap, bots and analytics

--- src/app/(frontend)/layout.jsx
+++ src/app/(frontend)/layout.jsx
@@ -1,8 +1,8 @@
-import "@mantine/core/styles.css";
-import "@mantine/notifications/styles.css";
-import "@/globals.css";
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+import '@/globals.css'
 
-import { Notifications } from "@mantine/notifications";
+import { Notifications } from '@mantine/notifications'
 
 import {
   ColorSchemeScript,
@@ -10,51 +10,65 @@ import {
   MantineProvider,
   createTheme,
   mantineHtmlProps,
-} from "@mantine/core";
+} from '@mantine/core'
 
 export const metadata = {
-  title: "VivaTDAH",
-  description: "A melhor plataforma TDAH do Brasil",
-};
+  title: 'VivaTDAH',
+  description: 'A melhor plataforma TDAH do Brasil',
+}
 
 const theme = createTheme({
   /* Custom colors */
   colors: {
-    "viva-orange": [
-      "#fff6e0",
-      "#ffebca",
-      "#ffd699",
-      "#ffbf63",
-      "#ffac36",
-      "#ffa018",
-      "#ff9904",
-      "#e48500",
-      "#cb7500",
-      "#b16400",
+    'viva-orange': [
+      '#fff6e0',
+      '#ffebca',
+      '#ffd699',
+      '#ffbf63',
+      '#ffac36',
+      '#ffa018',
+      '#ff9904',
+      '#e48500',
+      '#cb7500',
+      '#b16400',
     ],
-    "viva-purple": [
-      "#eef0fc",
-      "#d8dcf3",
-      "#adb5e9",
-      "#808cdf",
-      "#5a69d8",
-      "#4453d3",
-      "#3848d3",
-      "#2b3abb",
-      "#2433a7",
-      "#1a2b93",
+    'viva-purple': [
+      '#eef0fc',
+      '#d8dcf3',
+      '#adb5e9',
+      '#808cdf',
+      '#5a69d8',
+      '#4453d3',
+      '#3848d3',
+      '#2b3abb',
+      '#2433a7',
+      '#1a2b93',
     ],
   },
-  defaultRadius: "md",
-  primaryColor: "viva-orange",
-});
+  defaultRadius: 'md',
+  primaryColor: 'viva-orange',
+})
 
 export default function RootLayout({ children }) {
-  const maintenanceMode = false;
+  const maintenanceMode = false
 
   return (
     <html lang="pt-BR" {...mantineHtmlProps}>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GWMRV34BQ7" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-GWMRV34BQ7');
+            `,
+          }}
+        />
+
         <ColorSchemeScript />
       </head>
       <body>
@@ -62,11 +76,7 @@ export default function RootLayout({ children }) {
           {maintenanceMode ? (
             <div className="h-screen w-full flex justify-center items-center bg-white">
               <div className="space-y-4">
-                <img
-                  src="/logo.svg"
-                  alt="VivaTDAH"
-                  className="h-28 w-auto mx-auto"
-                />
+                <img src="/logo.svg" alt="VivaTDAH" className="h-28 w-auto mx-auto" />
                 <Divider />
                 <h1 className="text-xl font-bold">Voltamos em breve</h1>
               </div>
@@ -78,5 +88,5 @@ export default function RootLayout({ children }) {
         </MantineProvider>
       </body>
     </html>
-  );
+  )
 }
