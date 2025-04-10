f33697a84179a07aa4d101a2439c50c61f3e8fab
Alan Silva <alanwvsilva@gmail.com>
2025-04-10 20:50:37 +0100

    update cookies

--- src/app/(frontend)/(public)/(website)/_components/CookieConsent.jsx
+++ src/app/(frontend)/(public)/(website)/_components/CookieConsent.jsx
@@ -1,35 +1,38 @@
+'use client'
+
 import { Button } from '@mantine/core'
-import React from 'react'
+import { useState } from 'react'
 
 export default function CookieConsent() {
+  const [cookiesAllowed, setCookiesAllowed] = useState(
+    localStorage.getItem('cookieConsent') === 'true',
+  )
+
+  function acceptCookies() {
+    localStorage.setItem('cookieConsent', 'true')
+    setCookiesAllowed(true)
+  }
+
+  if (cookiesAllowed) {
+    return null
+  }
+
   return (
     <div className="fixed bottom-0 right-0 mb-4 mr-4 w-64">
       <div className="bg-[var(--mantine-color-body)] rounded-lg border shadow-lg p-4">
         <div className="flex items-center justify-between mb-2">
           <div className="flex items-center">
-            <img
-              src="https://www.svgrepo.com/show/401340/cookie.svg"
-              alt="Cookie"
-              className="h-6 w-6 mr-2"
-            />
-            <span className=" font-bold text-sm">Cookie Policy</span>
+            <img src="/cookie.svg" alt="Cookie" className="h-6 w-6 mr-2" />
+            <span className=" font-bold text-sm">Cookies</span>
           </div>
-          <button className="text-gray-500 hover:text-gray-700 focus:outline-none">
-            <svg className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              ></path>
-            </svg>
-          </button>
         </div>
         <p className="text-sm mb-2">
-          We use cookies to enhance your experience. By continuing to visit this site, you agree to
-          our use of cookies.
+          Utilizamos cookies para melhorar a sua experiência. Ao continuar navegando neste site,
+          você concorda com o uso de cookies.
         </p>
-        <Button>Accept</Button>
+        <Button className="w-full" onClick={acceptCookies}>
+          Accept
+        </Button>
       </div>
     </div>
   )
