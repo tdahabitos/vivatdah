befcca43cedee3ccdf965b60d542ae5c589eba18
Alan Silva <alanwvsilva@gmail.com>
2025-05-01 12:27:15 -0300

    update categories search

--- api/src/routes/categories.ts
+++ api/src/routes/categories.ts
@@ -1,62 +1,68 @@
-import express from "express";
-import axios from "axios";
-import qs from "qs";
+import express from 'express'
+import axios from 'axios'
+import qs from 'qs'
+import { Category } from '@/types'
 
-export const categoriesRouter = express.Router();
+export const categoriesRouter = express.Router()
 
-categoriesRouter.get("/categories", async (req, res) => {
-  const { free_content } = req.query;
+categoriesRouter.get('/categories', async (req, res) => {
+  const { free_content } = req.query
 
   const query = qs.stringify({
     where: {
       status: {
-        equals: "published",
+        equals: 'published',
       },
-      free_content: { equals: free_content === "true" },
+      free_content: { equals: free_content === 'true' },
     },
-    sort: ["-title"],
-  });
+  })
 
   const categories = await axios
     .get(`${process.env.CMS_API_URL}/categories?${query}`, {
       headers: {
-        accept: "application/json",
+        accept: 'application/json',
         Authorization: process.env.CMS_API_KEY,
       },
     })
-    .then((res) => res.data.docs);
+    .then((res) => res.data.docs)
 
-  res.status(200).json(categories);
-});
+  res
+    .status(200)
+    .json(
+      categories.sort((a: Category, b: Category) =>
+        a.title.localeCompare(b.title)
+      )
+    )
+})
 
-categoriesRouter.get("/categories/:id", async (req, res) => {
-  const { id } = req.params;
+categoriesRouter.get('/categories/:id', async (req, res) => {
+  const { id } = req.params
 
   const query = qs.stringify({
     where: {
       status: {
-        equals: "published",
+        equals: 'published',
       },
       id: {
         equals: id,
       },
     },
-  });
+  })
 
   const category = await axios
     .get(`${process.env.CMS_API_URL}/categories?${query}`, {
       headers: {
-        accept: "application/json",
+        accept: 'application/json',
         Authorization: process.env.CMS_API_KEY,
       },
     })
-    .then((res) => res.data.docs[0]);
+    .then((res) => res.data.docs[0])
 
-  res.status(200).json(category);
-});
+  res.status(200).json(category)
+})
 
-categoriesRouter.get("/categories/:id/media", async (req, res) => {
-  const { id } = req.params;
+categoriesRouter.get('/categories/:id/media', async (req, res) => {
+  const { id } = req.params
 
   const query = qs.stringify({
     where: {
@@ -67,16 +73,16 @@ categoriesRouter.get("/categories/:id/media", async (req, res) => {
         contains: id,
       },
     },
-  });
+  })
 
   const media = await axios
     .get(`${process.env.CMS_API_URL}/media?${query}`, {
       headers: {
-        accept: "application/json",
+        accept: 'application/json',
         Authorization: process.env.CMS_API_KEY,
       },
     })
-    .then((res) => res.data.docs);
+    .then((res) => res.data.docs)
 
-  res.status(200).json(media);
-});
+  res.status(200).json(media)
+})
